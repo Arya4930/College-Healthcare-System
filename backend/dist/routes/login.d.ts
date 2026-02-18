86615d6825d72d1468a7c148beeb8af94a261835
Arya <aryapanwar493@gmail.com>
2026-02-18 21:30:30 +0530

    gitignore update

--- backend/dist/routes/login.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Router } from "express";
-export declare const router: Router;
-export default router;
-//# sourceMappingURL=login.d.ts.map
--- backend/dist/routes/verify.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from "express";
-export declare const router: Router;
-export default router;
-//# sourceMappingURL=verify.d.ts.map
